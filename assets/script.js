b6a467a0d44e86f72de1133c27882f1c1c1239b9
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-03-04 12:42:52 -0500

    Now populates HTML, search functionality working

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,10 @@
-// function getApi() {
-//   var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=2c29190c1619626977d7f8a02bf6b35e';
+let city = document.querySelector('#cityInput');
+let searchBtn = document.querySelector('#searchBtn');
 
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data)
-//     });
-// }
-
-var city = document.querySelectorAll('#cityInput');
-var searchBtn = document.querySelectorAll('#searchBtn');
 
 const apiKey = "2c29190c1619626977d7f8a02bf6b35e";
 function weather() {
-  requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=${apiKey}&cnt=5
+  requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial&appid=${apiKey}
   `
   fetch(requestUrl)
   .then((response) => {
@@ -26,9 +15,26 @@ function weather() {
     return response.json();
   })
   .then((data) => {
-    console.log(data)
+    console.log(data);
+    //destructures data
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { speed } = data.wind;
+    const { temp, humidity } = data.main;
+    // //populates HTML for current day
+    document.querySelector('#currentCity').innerHTML = name;
+    document.querySelector('#icon').src = src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = `Temperature: ${temp} &deg;F`;
+    document.querySelector('#wind').innerHTML = `Wind: ${speed} mp/h`;
+    document.querySelector('#humidity').innerHTML = `Humidity: ${humidity}%`;
   })
 }
 
-// searchBtn.addEventListener('click', weather());
-//requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${apiKey}&cnt=5`
+searchBtn.addEventListener('click', weather);
+
+// searchBtn.addEventListener('keyup', function(e) {
+//   if (e.key === "enter") {
+//     console.log(working);
+//   }
+// });
